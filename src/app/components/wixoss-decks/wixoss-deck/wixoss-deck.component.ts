d8ba81a75c16555ac2d6bc719a649eb45407583a
steven.cruz <stiv.cruz6791@gmail.com>
2022-11-13 18:47:44 -0500

    create deck dto, create service classes

---
+++ src/app/components/wixoss-decks/wixoss-deck/wixoss-deck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wixoss-deck',
+  templateUrl: './wixoss-deck.component.html',
+  styleUrls: ['./wixoss-deck.component.scss']
+})
+export class WixossDeckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
