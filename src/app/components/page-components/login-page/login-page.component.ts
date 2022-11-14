b3de7b8fb3f951eaf27e25cf10f115e14db18091
steven.cruz <stiv.cruz6791@gmail.com>
2022-11-14 01:25:43 -0500

    Update DTO, themes, prelim login-register files

--- src/app/components/page-components/login-page/login-page.component.ts
+++ src/app/components/page-components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login-page',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm = new FormGroup({
+      emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+      usernameFormControl: new FormControl('', [Validators.required]),
+      passwordFormControl: new FormControl('', [Validators.required]),
+      confirmPasswordFormControl: new FormControl('', [Validators.required, this.passwordMatchingValidatior]),
+    });
+  
   }
 
+  
+
+  validateForm() {
+    if (this.userForm.controls['emailFormControl'].errors) {
+      console.log(this.userForm)
+      console.log(this.userForm?.hasError('notMatchingPasswords'));
+    }
+  }
+
+  passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const parentForm: FormGroup | FormArray | null = control?.parent;
+    const password: AbstractControl | null | undefined = parentForm?.get('passwordFormControl');
+    const confirmPassword: AbstractControl | null | undefined = parentForm?.get('confirmPasswordFormControl');
+    return password?.value === confirmPassword?.value ? null : {notMatchingPasswords: true};
+  };
 }
--- src/app/components/shared/app-material.module.ts
+++ src/app/components/shared/app-material.module.ts
@@ -3,19 +3,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
   @NgModule({
     imports: [
       MatToolbarModule,
       MatIconModule,
       MatButtonModule,
-      MatCardModule
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      FormsModule,
+      ReactiveFormsModule
     ],
     exports: [
       MatToolbarModule,
       MatIconModule,
       MatButtonModule,
-      MatCardModule
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      FormsModule,
+      ReactiveFormsModule
     ]
   })
   export class AppMaterialModule { }
