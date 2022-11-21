6bd3baf355116f27dd34a572af485d4356017192
steven.cruz <stiv.cruz6791@gmail.com>
2022-11-20 21:27:18 -0500

    update login-register page and style

--- src/app/components/login-register/login-register.component.ts
+++ src/app/components/login-register/login-register.component.ts
@@ -1,180 +1,228 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+    AbstractControl,
+    FormArray,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators, } from '@angular/forms';
 import { AuthDataService } from 'src/app/services/authentication/auth-data.service';
 import { UserRequest } from 'src/app/dto/payloads/userRequest';
 import { LoginRequest } from 'src/app/dto/payloads/loginRequest';
 import { InterceptorMessageServiceService } from 'src/app/services/interceptor-message/interceptor-message-service.service';
-
+import { Subscription } from 'rxjs';
 
 class FormText {
-  headerText: string;
-  submitButtonText: string;
-  formHelperText: string;
-  redirectText: string;
+    headerText: string;
+    submitButtonText: string;
+    formHelperText: string;
+    redirectText: string;
 
-  constructor(headerText: string, submitButtonText: string, formHelperText: string, redirectText: string) {
-    this.headerText = headerText;
-    this.submitButtonText = submitButtonText;
-    this.formHelperText = formHelperText;
-    this.redirectText = redirectText;
-  }
+    constructor(
+        headerText: string,
+        submitButtonText: string,
+        formHelperText: string,
+        redirectText: string) {
+        this.headerText = headerText;
+        this.submitButtonText = submitButtonText;
+        this.formHelperText = formHelperText;
+        this.redirectText = redirectText;
+    }
 }
 
 class InterceptorMessage {
-  message: string = null;
-  status: number = null;
-  constructor(message?: string, status?: number) {
-    this.message = message;
-    this.status = status;
-  }
+    message: string = null;
+    status: number = null;
+    constructor(message?: string, status?: number) {
+        this.message = message;
+        this.status = status;
+    }
 }
 
 @Component({
-  selector: 'login-register',
-  templateUrl: './login-register.component.html',
-  styleUrls: ['./login-register.component.scss']
+    selector: 'login-register',
+    templateUrl: './login-register.component.html',
+    styleUrls: ['./login-register.component.scss']
 })
 export class LoginRegisterComponent implements OnInit {
-  formType = {
-    signIn: 'Sign In',
-    forgotPassword: 'Forgot Password',
-    register: 'Register'
-  }
+    formType = {
+        signIn: 'Sign In',
+        forgotPassword: 'Forgot Password',
+        register: 'Register',
+    };
 
-  @ViewChild('form') form: any;
+    @ViewChild('form') form: any;
 
-  authError = new InterceptorMessage();
-  newUserText: FormText = new FormText(this.formType.register, 'Submit', 'Have an account?', 'Sign in here');
-  signInText: FormText = new FormText(this.formType.signIn, 'Sign In', 'Need to Register?', 'Register here');
-  forgotPasswordText: FormText = new FormText(this.formType.forgotPassword, 'Send Recovery Email', 'Have an account?', 'Sign in here');
-  formText: FormText;
-  userForm: FormGroup;
-  signIn: boolean;
-  newUser: boolean;
-  forgotPassword: boolean;
+    authError = new InterceptorMessage();
+    newUserText: FormText = new FormText(
+        this.formType.register,
+        'Submit',
+        'Have an account?',
+        'Sign in here');
+    signInText: FormText = new FormText(
+        this.formType.signIn,
+        'Sign In',
+        'Need to Register?',
+        'Register here');
+    forgotPasswordText: FormText = new FormText(
+        this.formType.forgotPassword,
+        'Send Recovery Email',
+        'Have an account?',
+        'Sign in here');
+    formText: FormText;
+    userForm: FormGroup;
+    signIn: boolean;
+    newUser: boolean;
+    forgotPassword: boolean;
 
-  constructor(private authService: AuthDataService, private interceptorMsgService: InterceptorMessageServiceService) { }
+    interceptorMsgServiceSub: Subscription;
 
-  ngOnInit(): void {
-    this.newUser = false;
-    this.resetFormTo(this.formType.signIn);
-    // this.interceptorMsgService.message.subscribe(message => {
-    //   this.authError.message = message;
-    // });
+    constructor(
+        private authService: AuthDataService,
+        private interceptorMsgService: InterceptorMessageServiceService) {
 
-    this.interceptorMsgService.status.subscribe(status => {
-      this.authError.status = status;
-    });
-  }
-
-  ngOnDestroy(): void {
-    //this.interceptorMsgService.message.unsubscribe();
-    this.interceptorMsgService.status.unsubscribe();
-  }
-
-  onSubmit() {
-    if (this.newUser) {
-      const username = this.userForm.controls['usernameFormControl'].value;
-      const password = this.userForm.controls['passwordFormControl'].value;
-      const email = this.userForm.controls['emailFormControl'].value;
-      const newUserRequest = new UserRequest(username, email, password);
-      this.authService.registerNewUser(newUserRequest);
     }
-    else if (this.signIn) {
-      const email = this.userForm.controls['emailFormControl'].value;
-      const password = this.userForm.controls['passwordFormControl'].value;
-      const loginRequest: LoginRequest = new LoginRequest(email, password);
-      this.authService.login(loginRequest);
-    }
-  }
 
-  passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    const parentForm: FormGroup | FormArray | null = control?.parent;
-    const password: AbstractControl | null | undefined = parentForm?.get('passwordFormControl');
-    const confirmPassword: AbstractControl | null | undefined = parentForm?.get('confirmPasswordFormControl');
-    return password?.value === confirmPassword?.value ? null : {notMatchingPasswords: true};
-  };
-
-  onPasswordChange() {
-    if (this.newUser) { this.userForm.controls['confirmPasswordFormControl'].updateValueAndValidity(); }
-  }
-
-  getAuthErrorMsg(code: number): string {
-    let msg = null;
-    switch(code) {
-      case(401):
-        msg = "An invalid password was provided!";
-        break;
-      case(404):
-        msg = "An invalid email was provided!";
-        break;
-      case(409):
-        msg = "That email is already in use!";
-        break;
-      default:
-        msg = 'An unexpected error ocurred!';
-    }
-    return msg;
-  }
-
-  getRedirectLocation(): void {
-    this.signIn ? this.resetFormTo(this.formType.register) : this.resetFormTo(this.formType.signIn);
-  }
-
-  goToForgotPassword(): void {
-    this.resetFormTo(this.formType.forgotPassword);
-  }
-
-  getAuthErrorStatus(): boolean {
-    return (this.authError.status) ? true : false;
-  }
-
-  resetAuthError(): void {
-    this.interceptorMsgService.setStatus(null);
-    this.interceptorMsgService.setMessage(null);
-  }
-
-  resetFormTo(formType: string): void {
-    this.resetAuthError();
-    switch(formType) {
-      case this.formType.forgotPassword: {
-        this.formText = this.forgotPasswordText;
-        this.forgotPassword = true;
-        this.signIn = false;
+    ngOnInit(): void {
         this.newUser = false;
-        this.userForm = new FormGroup({
-          emailFormControl: new FormControl('', [Validators.required, Validators.email])});
-        break;
-      }
-      case this.formType.register: {
-        this.formText = this.newUserText;
-        this.forgotPassword = false;
-        this.signIn = false;
-        this.newUser = true;
-        this.userForm = new FormGroup({
-          emailFormControl: new FormControl('', [Validators.required, Validators.email]),
-          passwordFormControl: new FormControl('', [Validators.required]),
-          usernameFormControl: new FormControl('', [Validators.required]),
-          confirmPasswordFormControl: new FormControl('', [Validators.required, this.passwordMatchingValidatior])
-        });
-        break;
-      }
-      default: {
-        this.formText = this.signInText;
-        this.forgotPassword = false;
-        this.signIn = true;
-        this.newUser = false;
-        this.userForm = new FormGroup({
-          emailFormControl: new FormControl('', [Validators.required, Validators.email]),
-          passwordFormControl: new FormControl('', [Validators.required])
-        });
-        break;
-      }
-    }
-    this.form?.resetForm();
-  }
+        this.resetFormTo(this.formType.signIn);
+        this.interceptorMsgServiceSub =
+            this.interceptorMsgService.status.subscribe((status) => {
+                this.authError.status = status;
+            });
 
-  /* Notes
+        this.interceptorMsgServiceSub.add(
+            this.interceptorMsgService.message.subscribe((message) => {
+                this.authError.message = message;
+            })
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.interceptorMsgServiceSub.unsubscribe();
+    }
+
+    onSubmit() {
+        if (this.newUser) {
+            const username =
+                this.userForm.controls['usernameFormControl'].value;
+            const password =
+                this.userForm.controls['passwordFormControl'].value;
+            const email = this.userForm.controls['emailFormControl'].value;
+            const newUserRequest = new UserRequest(username, email, password);
+            this.authService
+                .registerNewUser(newUserRequest)
+                .subscribe((resp) => {
+                    // Redirect user to home page and be logged in
+                    // OR
+                    // Give toast of some sort to user and reset form to Sign in
+                    console.log('in login register', resp);
+                });
+        } else if (this.signIn) {
+            const email = this.userForm.controls['emailFormControl'].value;
+            const password =
+                this.userForm.controls['passwordFormControl'].value;
+            const loginRequest: LoginRequest = new LoginRequest(
+                email,
+                password
+            );
+            this.authService.login(loginRequest);
+        }
+    }
+
+    passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+        const parentForm: FormGroup | FormArray | null = control?.parent;
+        const password: AbstractControl | null | undefined = parentForm?.get('passwordFormControl');
+        const confirmPassword: AbstractControl | null | undefined =
+            parentForm?.get('confirmPasswordFormControl');
+        return password?.value === confirmPassword?.value ? null : { notMatchingPasswords: true };
+    };
+
+    onPasswordChange() {
+        if (this.newUser) {
+            this.userForm.controls['confirmPasswordFormControl'].updateValueAndValidity();
+        }
+    }
+
+    getAuthErrorMsg(code: number): string {
+        let msg = null;
+        switch (code) {
+            case 401:
+                msg = 'An invalid password was provided!';
+                break;
+            case 404:
+                msg = 'An invalid email was provided!';
+                break;
+            case 409:
+                msg = 'That email is already in use!';
+                break;
+            default:
+                msg = this.authError.message;
+        }
+        return msg;
+    }
+
+    getRedirectLocation(): void {
+        this.signIn ? this.resetFormTo(this.formType.register) : this.resetFormTo(this.formType.signIn);
+    }
+
+    goToForgotPassword(): void {
+        this.resetFormTo(this.formType.forgotPassword);
+    }
+
+    getAuthErrorStatus(): boolean {
+        return this.authError.status ? true : false;
+    }
+
+    resetAuthError(): void {
+        this.interceptorMsgService.setStatus(null);
+        this.interceptorMsgService.setMessage(null);
+    }
+
+    resetFormTo(formType: string): void {
+        this.resetAuthError();
+        switch (formType) {
+            case this.formType.forgotPassword: {
+                this.formText = this.forgotPasswordText;
+                this.forgotPassword = true;
+                this.signIn = false;
+                this.newUser = false;
+                this.userForm = new FormGroup({
+                    emailFormControl: new FormControl('', [Validators.required, Validators.email])
+                });
+                break;
+            }
+            case this.formType.register: {
+                this.formText = this.newUserText;
+                this.forgotPassword = false;
+                this.signIn = false;
+                this.newUser = true;
+                this.userForm = new FormGroup({
+                    emailFormControl: new FormControl('', [Validators.required,Validators.email,]),
+                    passwordFormControl: new FormControl('', [Validators.required]),
+                    usernameFormControl: new FormControl('', [Validators.required]),
+                    confirmPasswordFormControl: new FormControl('', [Validators.required, this.passwordMatchingValidatior])
+                });
+                break;
+            }
+            default: {
+                this.formText = this.signInText;
+                this.forgotPassword = false;
+                this.signIn = true;
+                this.newUser = false;
+                this.userForm = new FormGroup({
+                    emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+                    passwordFormControl: new FormControl('', [Validators.required])
+                });
+                break;
+            }
+        }
+        this.form?.resetForm();
+    }
+
+    /* Notes
     Before I could use the below to add FormControls to the FormGroup.
     this.userForm.addControl('usernameFormControl', this.formBuilder.control('', [Validators.required]));
     this.userForm.addControl('confirmPasswordFormControl', this.formBuilder.control('', [Validators.required, this.passwordMatchingValidatior]));
